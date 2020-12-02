ce45550feb12a20a4628bc43aef0bca87b88e447
Kinfull <73040766+Kinfull@users.noreply.github.com>
2020-12-02 12:33:48 +0100

    Update 2.py

--- 2020/2/2.py
+++ 2020/2/2.py
@@ -1,23 +1,50 @@
 with open(r"2020\2\2.txt", "r") as f:
-    passwordDict = [cords.rstrip("None") for cords in f.readlines()]
+    passwordList = [lines.split(":") for lines in f.readlines()]
+count = 0
+"""
 
-
-def PasswordFix(input_list):
-    array = input_list[:]
+for passwords in passwordList:
+    temp_count = 0
+    #temp_count = amount of times rule occurs in password
+    numbers = passwords[0].split("-")
     
-    for index in array:
-        """
-        if(index[1] == "-"):
-            lowerRange = index[0]
-        else:
-            lowerRange = index[0:1]
-        if(index[3] == " "):
-            upperRange = index[2]
-        else:
-            upperRange = index[2:3]
-        if(index[4] != " "): 
-            policy = index[4]
-        else:
-            policy = index[5]"""
+    numbersLow = numbers[0]
+    #numbersLow = lowest number it can contain
+    
+    numbersTemp = numbers[1].split()
+    numbersHigh = numbersTemp[0]
+    #numbersHigh = Highest number it can contain
+    
+    rule = numbersTemp[1]
+    #rule = what character it has to contain
 
-PasswordFix(passwordDict)
+    #tests how many times the rule occurs in password (+= temp_count) for evet time it does
+    for j in range(len(passwords[1])-1):
+        if(rule == passwords[1][j]):
+            temp_count += 1
+
+    # if temp_count is within numbersLow an numbersHigh += count (password is valid)
+    if((temp_count >= int(numbersLow)) and (temp_count <= int(numbersHigh))):
+        count += 1
+print(count)
+"""
+
+for passwords in passwordList:
+    temp_count = 0
+    #temp_count = amount of times rule occurs in password
+    numbers = passwords[0].split("-")
+    
+    numbersLow = numbers[0]
+    #numbersLow = lowest number it can contain
+    
+    numbersTemp = numbers[1].split()
+    numbersHigh = numbersTemp[0]
+    #numbersHigh = Highest number it can contain
+    
+    rule = numbersTemp[1]
+    #rule = what character it has to contain
+
+    if(((rule == passwords[1][int(numbersLow)+1]) or (rule == passwords[1][int(numbersHigh)+1])) and (passwords[1][int(numbersLow)+1] != passwords[1][int(numbersHigh)+1])):
+        count += 1
+
+print(count)
